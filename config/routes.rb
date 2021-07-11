cfaca0b94dbdeb48ac9cb355885351972cd12bf4
MYK28 <myams1128@gmail.com>
2021-07-11 12:58:14 +0000

    [#16]footerの作成(仮)

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     passwords: 'admins/passwords',
     registrations: 'admins/registrations'
   }
-  
+
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
