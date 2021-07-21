eed6cead0fdba4fcadb18d4d99df4d1a638c01bc
MYK28 <myams1128@gmail.com>
2021-07-21 07:47:12 +0000

    [#60](staff)ブックマーク機能の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_083025) do
+ActiveRecord::Schema.define(version: 2021_07_21_042210) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2021_07_18_083025) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "staff_id", null: false
+    t.integer "information_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["information_id"], name: "index_bookmarks_on_information_id"
+    t.index ["staff_id"], name: "index_bookmarks_on_staff_id"
   end
 
   create_table "checks", force: :cascade do |t|
