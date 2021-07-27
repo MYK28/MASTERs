c504c714fb8dc8b7a10d2a12cc6b5910fb11d88c
MYK28 <myams1128@gmail.com>
2021-07-27 16:39:12 +0000

    [#59](staff)レイアウトの編集

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
   }
 
   root to: 'homes#top'
-  get 'home/about' => 'homes#about'
 
   resources :informations, only: [:index, :show] do
     delete 'check' => 'checks#destroy'
